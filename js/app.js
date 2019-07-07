e5029024bac5b273a710a0a02c1a8a8c590a2d6f
Casper <casper.thecuteghost@gmail.com>
2019-07-07 16:46:21 +0200

    Added keyboard functionality

--- js/app.js
+++ js/app.js
@@ -27,6 +27,36 @@
 			showResult();
 		});
 	})
+	document.addEventListener('keydown',function(event){
+		let key = event.keyCode - 96;
+		console.log(key);
+		buttons.forEach(function(button){
+			if(key == button.attributes[0].value[6]){
+				button.click();
+			}
+		})
+		if(key == 11){
+			document.querySelector('.nplus').click();
+		}
+		else if(key == 13){
+			document.querySelector('.nminus').click();
+		}
+		else if(key == 10){
+			document.querySelector('.nmul').click();
+		}
+		else if(key == 15){
+			document.querySelector('.ndiv').click();
+		}
+		else if(key == -29){
+			document.querySelector('.nc').click();
+		}
+		else if(key == -83){
+			document.querySelector('.nequal').click();
+		}
+		else if(key == 14 || key == 94){
+			document.querySelector('.ndot').click();
+		}
+	})
 
 	function showResult(){
 		resultScreen.innerText = result;
