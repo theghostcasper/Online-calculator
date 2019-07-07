6f8f9f3b08aec94674c63c083fa48de0be4afd6a
Casper <casper.thecuteghost@gmail.com>
2019-07-07 15:49:51 +0200

    refactoring some js code

--- js/app.js
+++ js/app.js
@@ -1,48 +1,47 @@
-let calcArray = []
-let result = 0;
-let buttons = Array.from(document.querySelectorAll('button'))
-buttons.forEach(function(button){
-	button.addEventListener('click',function(event){
-		if(event.target.innerText == 'C')
-		{
-			calcArray = [];
-			result = '';
-			showOperation();
-			showResult();
-		}
-		else if(event.target.innerText != '='){
-			calcArray.push(event.target.innerText)
-			showOperation();
-			showResult();			
-		}
-		if(event.target.innerText == '='){
-			calcArray = [calcArray.calc()];
-			if(calcArray[0] == undefined)
-				calcArray[0]='';
-			if(result == undefined)
+(function(){
+	let calcArray = []
+	let result = 0;
+	let buttons = Array.from(document.querySelectorAll('button'))
+	let resultScreen = document.querySelector('#resultScreen');
+	let operationScreen = document.querySelector('#operationScreen');
+
+
+	buttons.forEach(function(button){
+		button.addEventListener('click',function(event){
+			if(event.target.innerText == 'C')
+			{
+				calcArray = [];
 				result = '';
+			}
+			else if(event.target.innerText != '='){
+				calcArray.push(event.target.innerText)			
+			}
+			if(event.target.innerText == '='){
+				calcArray = [calcArray.calc()];
+				if(calcArray[0] == undefined)
+					calcArray[0]='';
+				if(result == undefined)
+					result = '';
+			}
 			showOperation();
 			showResult();
-		}
-	});
-})
+		});
+	})
 
+	function showResult(){
+		resultScreen.innerText = result;
+	}
+	function showOperation(){
+		let operations = calcArray.join('');
+		operationScreen.innerText = operations;
+	}
 
-function showResult(){
-	let screen = document.querySelector('#resultScreen');
-	screen.innerText = result;
-}
-function showOperation(){
-	let screen = document.querySelector('#operationScreen');
-	let operations = calcArray.join('');
-	screen.innerText = operations;
-}
-
-function calc(){
-	if(this[this.length-1] > '9' || this[this.length-1] < '0' )
-		this.pop();
-	result = eval(this.join(''));
-	showResult();
-	return result;
-}
-calcArray.__proto__.calc  = calc;
+	function calc(){
+		if(this[this.length-1] > '9' || this[this.length-1] < '0' )
+			this.pop();
+		result = eval(this.join(''));
+		showResult();
+		return result;
+	}
+	calcArray.__proto__.calc  = calc;
+}())
